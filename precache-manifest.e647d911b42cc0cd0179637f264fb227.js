3659bad234c3ef63c86e3eb107008d0052396a78
Luthien95 <aleksandrasalak@gmail.com>
2020-09-18 01:29:58 +0200

    Updates

---
+++ precache-manifest.e647d911b42cc0cd0179637f264fb227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed3f00b32c375cb28f5b3297eece6f9",
+    "url": "/new-netflix/index.html"
+  },
+  {
+    "revision": "5e812965a45c3678ee1c",
+    "url": "/new-netflix/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2c276c0dbe0ff6d26e6e",
+    "url": "/new-netflix/static/css/main.7c2f50ed.chunk.css"
+  },
+  {
+    "revision": "5e812965a45c3678ee1c",
+    "url": "/new-netflix/static/js/2.d6eccf9d.chunk.js"
+  },
+  {
+    "revision": "80097c1312432faada68f848f38927c4",
+    "url": "/new-netflix/static/js/2.d6eccf9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c276c0dbe0ff6d26e6e",
+    "url": "/new-netflix/static/js/main.e63cce21.chunk.js"
+  },
+  {
+    "revision": "610b3bd264c3570676cc",
+    "url": "/new-netflix/static/js/runtime-main.afec9021.js"
+  }
+]);
